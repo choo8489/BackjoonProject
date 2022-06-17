03ebe354a43f356936541419efde8a530b7dce04
djlee <djlee@fxgear.net>
2022-06-17 11:37:15 +0900

    1003, 1004 문제 해결 업로드

--- BackjoonProject/Question.cs
+++ BackjoonProject/Question.cs
@@ -66,5 +66,84 @@ namespace BackjoonProject
                 }
             }
         }
+
+        // 피보나치 메모이제이션
+        public void Q1003()
+        {
+            int[,] fibo = new int[41, 2];
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                Fibonacci(n);
+                Console.WriteLine($"{fibo[n, 0]} {fibo[n, 1]}");
+            }
+
+            (int, int) Fibonacci(int n)
+            {
+                if (n == 0)
+                {
+                    fibo[n, 0] = 1;
+                    fibo[n, 1] = 0;
+                    return (fibo[n, 0], fibo[n, 1]);
+                }
+                else if (n == 1)
+                {
+                    fibo[n, 0] = 0;
+                    fibo[n, 1] = 1;
+                    return (fibo[n, 0], fibo[n, 1]);
+                }
+
+                if (fibo[n, 0] != 0 && fibo[n, 1] != 0)
+                {
+                    return (fibo[n, 0], fibo[n, 1]);
+                }
+                else
+                {
+                    fibo[n, 0] = Fibonacci(n - 1).Item1 + Fibonacci(n - 2).Item1;
+                    fibo[n, 1] = Fibonacci(n - 1).Item2 + Fibonacci(n - 2).Item2;
+                    return (fibo[n, 0], fibo[n, 1]);
+                }
+            }
+        }
+
+        public void Q1004 ()
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                int count = 0;
+                string[] n = Console.ReadLine().Split();
+
+                (int x1, int y1) = (int.Parse(n[0]), int.Parse(n[1]));
+                (int x2, int y2) = (int.Parse(n[2]), int.Parse(n[3]));
+
+                int planetCount = Convert.ToInt32(Console.ReadLine());
+
+                for (int j = 0; j < planetCount; j++)
+                {
+                    string[] planetInfo = Console.ReadLine().Split();
+
+                    (int cx, int cy, int r) = (int.Parse(planetInfo[0]), int.Parse(planetInfo[1]), int.Parse(planetInfo[2]));
+
+                    double distance1 = Math.Pow(x1 - cx, 2) + Math.Pow(y1 - cy, 2);
+                    double distance2 = Math.Pow(x2 - cx, 2) + Math.Pow(y2 - cy, 2);
+
+                    double powR = r * r;
+
+                    // 만약 두 거리가 모두 행성의 반지름 보다 작다면, 출발점과 시작점 모두 행성 내부에 있기 때문에 진입 x
+                    // 두 지점 중 하나만 행성 내부에 있는 경우에는 진입 또는 이탈 필요
+                    if (powR > distance1 && powR > distance2)
+                        continue;
+                    else if (powR > distance1)
+                        count++;
+                    else if (powR > distance2)
+                        count++;
+                }
+
+                Console.WriteLine(count);
+            }
+        }
     }
 }
