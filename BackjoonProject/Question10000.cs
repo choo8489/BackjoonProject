f607ce6db92720f0b4dd7954dd31a2f6f38d9097
choo8489 <choo9959@gmail.com>
2022-07-08 19:40:40 +0900

    15829 문제 풀이 ( 해쉬 )

--- BackjoonProject/Question10000.cs
+++ BackjoonProject/Question10000.cs
@@ -868,6 +868,37 @@ namespace BackjoonProject
             Console.WriteLine(builder);
         }
 
+        public void Q15829()
+        {
+            StreamWriter writer = new StreamWriter(Console.OpenStandardOutput());
+            StreamReader reader = new StreamReader(Console.OpenStandardInput());
+
+            int l = int.Parse(reader.ReadLine());
+            string str = reader.ReadLine();
+
+            long result = 0;
+            long R = 1;
+            long MOD = 1234567891;
+
+            for (int i = 0; i < l; i++)
+            {
+                result += ((int)str[i] - 'a' + 1) * R;
+
+                // 덧셈하는 과정에서 소수 M으로 나누어 줘야 값이 너무 커지지 않습니다.
+                // (a + b) mod n = {(a mod n) + (b mod n)} mod n
+                result %= MOD;
+
+                R *= 31;
+                R %= MOD;
+            }
+
+
+            writer.WriteLine(result);
+
+            writer.Close();
+            reader.Close();
+        }
+
         public void Q17478()
         {
             // 문제를 틀렸습니다. ( 나중에 문제 해결 할예정 )
